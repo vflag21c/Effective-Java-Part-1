5f3b99898319fee2c8f17a57b66d31965c1c3b13
유열 <vflag21c@nate.com>
2024-11-18 23:44:22 +0900

    Effective Java - 인터페이스와 정적 메소드

--- src/main/java/example/chapter01/item01/HelloService.java
+++ src/main/java/example/chapter01/item01/HelloService.java
@@ -17,19 +17,24 @@ public interface HelloService {
         return "hi";
     }
 
+    // 자바 9 부터 지원
     static private void prepareMessage() {
+        //공통적인 작업 ...
     }
 
+    // public 생략
     static String hi1() {
         prepareMessage();
         return "hi";
     }
 
+    //1. 인스턴스 생성 하지 않고도 제공할 것이냐
     static String hi2() {
         prepareMessage();
         return "hi";
     }
 
+    //2. 인스턴스 생성후 메소드 제공 할 것이냐
     default String bye() {
         return "bye";
     }
