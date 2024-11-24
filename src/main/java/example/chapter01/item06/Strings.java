83c25501435403837c0a3934859a0d0aeb17769b
유열 <vflag21c@nate.com>
2024-11-24 22:24:07 +0900

    Effective Java - 불필요한 객체 생성

---
+++ src/main/java/example/chapter01/item06/Strings.java
@@ -0,0 +1,14 @@
+package example.chapter01.item06;
+
+public class Strings {
+    public static void main(String[] args) {
+        String hello = "hello";
+        String hello2 = new String("hello");
+        String hello3 = "hello";
+
+        System.out.println(hello == hello2);
+        System.out.println(hello.equals(hello2));
+        System.out.println(hello == hello3);
+        System.out.println(hello.equals(hello3));
+    }
+}
---
+++ src/main/java/example/chapter01/item06/Sum.java
@@ -0,0 +1,19 @@
+package example.chapter01.item06;
+
+public class Sum {
+    private static long sum() {
+        // Long을 long으로 변경하여 실행
+        long sum = 0L;
+        for (long i = 0; i <= Integer.MAX_VALUE; i++)
+            sum += i;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        long start = System.nanoTime();
+        long x = sum();
+        long end = System.nanoTime();
+        System.out.println((end - start) / 1_000_000. + " ms.");
+        System.out.println(x);
+    }
+}
