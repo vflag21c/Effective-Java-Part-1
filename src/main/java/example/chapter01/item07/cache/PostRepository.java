403ecf3d1f3120002ec3a44a64585ee9e91cfa54
유열 <vflag21c@nate.com>
2024-11-26 22:49:15 +0900

    Effective Java - 객체 참조 해제

---
+++ src/main/java/example/chapter01/item07/cache/PostRepository.java
@@ -0,0 +1,30 @@
+package example.chapter01.item07.cache;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.WeakHashMap;
+
+public class PostRepository {
+
+    private Map<CacheKey, Post> cache;
+
+    public PostRepository() {
+        this.cache = new WeakHashMap<>(); //weakHashMap 사용
+//        this.cache = new HashMap<>();
+    }
+
+    public Post getPostById(CacheKey key) {
+        if (cache.containsKey(key)) {
+            return cache.get(key);
+        } else {
+            // TODO DB에서 읽어오거나 REST API를 통해 읽어올 수 있습니다.
+            Post post = new Post();
+            cache.put(key, post);
+            return post;
+        }
+    }
+
+    public Map<CacheKey, Post> getCache() {
+        return cache;
+    }
+}
---
+++ src/main/java/example/chapter01/item07/stack/EmptyStackException.java
@@ -0,0 +1,4 @@
+package example.chapter01.item07.stack;
+
+public class EmptyStackException extends IllegalStateException {
+}
