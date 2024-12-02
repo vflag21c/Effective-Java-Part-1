8fe8b03ba3a26b84c73c275ecb7945e34651491b
유열 <vflag21c@nate.com>
2024-12-02 21:56:23 +0900

    Effective Java - Value 기반 클래스

--- src/main/java/example/chapter02/item10/record/Point.java
+++ src/main/java/example/chapter02/item10/record/Point.java
@@ -1,5 +1,10 @@
-package example.chapter02.item10.record;//package me.whiteship.chapter02.item10.record;
-//
-//public record Point(int x, int y) {
-//
-//}
+package example.chapter02.item10.record;
+
+///equals & hashCode 를 재정의 해 줄 필요가 없다.
+/// toString 도 마찬가지.
+public record Point(int x, int y) {
+
+    public String hello() {
+        return "hello";
+    }
+}
